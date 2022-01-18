7564106215b401e66c682bf2e64c77bd69d24591
Divine Orji <dpkreativ@gmail.com>
2022-01-18 12:36:15 +0100

    display blog content on homepage by title

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1 class="text-7xl font-black text-center">New on the blog</h1>
+    <div v-for="post in posts" :key="post.title">
+      <h2>{{ post.title }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
